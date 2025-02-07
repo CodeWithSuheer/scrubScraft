b8a2f96e4a93735a974c6b50eb19c3d2ff070d25
Suheer <glowingemp124@glowingsoft.com>
2025-02-07 22:15:21 +0500

    pagination bar color updated

--- src/sections/products/all-products.tsx
+++ src/sections/products/all-products.tsx
@@ -116,7 +116,7 @@ const AllProducts: React.FC = () => {
                         <Link
                           onClick={ToTop}
                           to={`/products?category=${category}&page=${page - 1}`}
-                          className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                          className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700"
                         >
                           <span className="sr-only">Previous</span>
                           <svg
@@ -165,7 +165,7 @@ const AllProducts: React.FC = () => {
                         <Link
                           onClick={ToTop}
                           to={`/products?category=${category}&page=${page + 1}`}
-                          className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                          className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700"
                         >
                           <span className="sr-only">Next</span>
                           <svg
